47be3032cd48f70baab228aec938bd851ac8e7e1
Rajat Kumar <kumar.rajatprajapati@gmail.com>
2020-03-06 11:37:51 +0530

    added basic components of tree

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {faChevronRight} from "@fortawesome/free-solid-svg-icons";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [component, setComponent] = useState([
+        {
+            'id': 1,
+            'title': 'Component 1',
+            'opened': false,
+            'children': [
+                {
+                    'id': 1,
+                    'title': 'Component 1\'s children',
+                    'opened': false,
+                    'children': [
+                        {
+                            'id': 1,
+                            'title': 'Yet another children',
+                            'opened': false,
+                            'children': null,
+                        }
+                    ],
+                }
+            ]
+        }
+    ]);
+
+    return (
+        <div className="App">
+            {component.map((c, index) =>
+                <SingleComponent
+                    id={c.id}
+                    title={c.title}
+                    children={c.children}
+                    opened={c.opened}
+                />
+            )
+            }
+        </div>
+    );
 }
+
+
+function SingleComponent(props) {
+    return (
+        <div>
+            <div className="single-component">
+                {props.children != null &&
+                <FontAwesomeIcon
+                    className="fav-icons"
+                    icon={faChevronRight}
+                />
+                }
+                <div>{props.title}</div>
+                <FontAwesomeIcon
+                    className="fav-icons"
+                    icon={faPlus}
+                />
+            </div>
+            {props.children != null &&
+            <div className="child-component">
+                {props.children.map((c, index) =>
+                    <SingleComponent
+                        id={c.id}
+                        title={c.title}
+                        children={c.children}
+                        opened={false}
+                    />
+                )
+                }
+            </div>
+            }
+        </div>
+    );
+}
+
 
 export default App;
